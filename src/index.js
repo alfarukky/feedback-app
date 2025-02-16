76e7bc0de1ab856d477639f67a745fa60aa65897
alfarukky <alfarukky@gmail.com>
2025-02-17 03:01:32 +0400

    Header component removed from file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Header from './components/Header';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <Header text="Fedback UI" bgColor="red" textColor="blue" />
     <App />
   </React.StrictMode>
 );
