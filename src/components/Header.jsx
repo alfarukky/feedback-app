7e99ac2e42d6663f44ce82f6b07543ab12940140
alfarukky <alfarukky@gmail.com>
2025-02-16 18:34:23 +0400

    Header comment created

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import propTypes from 'prop-types';
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+Header.defaultProps = {
+  text: 'Feedback UI',
+};
+Header.propTypes = {
+  text: propTypes.string,
+};
+export default Header;
