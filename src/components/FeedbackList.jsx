fcc22c89733306dde7ae3d2ae3fca2b5b868fb19
alfarukky <alfarukky@gmail.com>
2025-03-08 20:03:37 +0400

    loading effect added to feedback list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,15 @@
 import { useContext } from 'react';
 import FeedbackItem from './feedbackItem';
+import Spinner from './shared/Spinner';
 import FeedbackContext from '../context/FeedbackContext';
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if ((!isLoading && !feedback) || feedback.length === 0) {
     return <p>No Feedback Yet </p>;
   }
-
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map((item) => (
         <FeedbackItem key={item.id} item={item} />
